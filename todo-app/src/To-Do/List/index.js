16b403958e5d870960dd407c9e99672832d4417a
HasanRs <hasanbaygul@outlook.com.tr>
2022-09-24 00:41:02 +0300

    update

--- todo-app/src/To-Do/List/index.js
+++ todo-app/src/To-Do/List/index.js
@@ -1,11 +1,54 @@
 import React from 'react'
 
 function List({ note, setToDoList, toDoList, deleteNote }) {
-  return (
-    <div>
-        
-    </div>
-  )
+    function deleteBtn() {
+        deleteNote(note.id)
+    }
+
+    function handleChecked() {
+        let checkedNote = toDoList.map((prevNote) => {
+            if (prevNote.id === note.id) {
+                prevNote.isChecked = !prevNote.isChecked
+            }
+            return prevNote
+        })
+
+        setToDoList(checkedNote)
+        localStorage.setItem("notes", JSON.stringify(checkedNote))
+    }
+
+    const handleUpdateNote = (e) => {
+        let updateNotes = toDoList.map((prevNote) => {
+            if (prevNote.id === note.id) {
+                prevNote.note = e.target.value
+            } 
+            return prevNote
+        })
+        localStorage.setItem("notes", JSON.stringify(updateNotes))
+        setToDoList(updateNotes)
+    }
+
+    let isTextCompleted = note.isChecked ? "completed" : ""
+
+    return (
+            <li className={isTextCompleted}>
+                <div className='view'>
+                    <input 
+                        className='toggle'
+                        type="checkbox"
+                        checked={note.isChecked}
+                        onChange={handleChecked}
+                    />
+                    <label onChange={handleUpdateNote}>
+                        <input 
+                            className='input-label'
+                            defaultValue={note.note}
+                        />
+                    </label>
+                    <button className='destroy' onClick={deleteBtn}></button>
+                </div>
+            </li>
+    )
 }
 
 export default List
--- todo-app/src/To-Do/todo.js
+++ todo-app/src/To-Do/todo.js
@@ -1,69 +1,107 @@
+import { useState, useEffect } from "react";
 import Input from "./Input";
 import List from "./List";
 
 
 function Todo() {
+    const [toDoList, setToDoList] = useState([])
+    const [status, setStatus] = useState("")
+    const [noteList, setNoteList] = useState([])
+
+    useEffect(() => {
+        const getLocalNotes = JSON.parse(localStorage.getItem("notes"))
+        setToDoList(getLocalNotes)
+        
+        if (getLocalNotes === null) {
+            setToDoList([])
+        } else {
+            setToDoList(getLocalNotes)
+        }
+    }, [])
+
+    useEffect(() => {
+        switch (status) {
+            case "All":
+                setNoteList(toDoList)
+                break;
+            case "Active":
+            let activeNote = toDoList.filter((note) => note.isChecked === false)
+            setNoteList(activeNote)
+                break;
+            case "Completed":
+                let completeNote = toDoList.filter((note) => note.isChecked === true)
+                setNoteList(completeNote)
+                break;
+            default:
+                setNoteList(toDoList)
+                break;
+        }
+    }, [toDoList, status])
+
+    function handleSubmit(value) {
+        setToDoList(prevNote => [...prevNote, value])
+        localStorage.setItem("notes", JSON.stringify([...toDoList, value]))
+    }
+
+    function deleteNote(delNoteId) {
+        let newList = toDoList.filter((prevNote) => prevNote.id !== delNoteId)
+        setToDoList(newList)
+        localStorage.setItem("notes", JSON.stringify(newList))
+    }
+
   return (
     <div>
-          <section className="todoapp">
-    <Input  />
-    <section className="main">
-      <input className="toggle-all" type="checkbox" />
-      <label htmlFor="toggle-all">Mark all as complete</label>
-      <ul className="todo-list">
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn JavaScript</label>
-            <button className="destroy" />
-          </div>
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn React</label>
-            <button className="destroy" />
-          </div>
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Have a life!</label>
-            <button className="destroy" />
-          </div>
-        </li>
-      </ul>
+        <section className="todoapp">
+        <Input />
+        <section className="main">
+            <input className="toggle-all" type="checkbox" checked={toDoList.find(note => note.isChecked === true) ? true : false} onChange={allChecked} />
+            {
+                toDoList.length <= 0 ? null : <label htmlFor="toggle-all" onClick={allChecked}>Mark all as complete</label>
+            }
+            <ul className="todo-list">
+                {
+                    noteList.map((note) => {
+                        return <List
+                            key={note.id}
+                            note={note}
+                            setToDoList={setNoteList}
+                            toDoList={toDoList}
+                            deleteNote={deleteNote}
+                        />
+                    })
+                }
+            </ul>
+        </section>
+        <footer className="footer">
+        <span className="todo-count">
+            <strong>2</strong>
+            items left
+        </span>
+        <ul className="filters">
+            <li>
+            <a href="#/" className="selected">
+                All
+            </a>
+            </li>
+            <li>
+            <a href="#/">Active</a>
+            </li>
+            <li>
+            <a href="#/">Completed</a>
+            </li>
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+        </footer>
     </section>
-    <footer className="footer">
-      <span className="todo-count">
-        <strong>2</strong>
-        items left
-      </span>
-      <ul className="filters">
-        <li>
-          <a href="#/" className="selected">
-            All
-          </a>
-        </li>
-        <li>
-          <a href="#/">Active</a>
-        </li>
-        <li>
-          <a href="#/">Completed</a>
-        </li>
-      </ul>
-      <button className="clear-completed">Clear completed</button>
+    <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>
+        Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
+        </p>
+        <p>
+        Edited to React App by <a href="https://github.com/HasanRs">@HasanRs</a>
+        </p>
     </footer>
-  </section>
-  <footer className="info">
-    <p>Click to edit a todo</p>
-    <p>
-      Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
-    </p>
-    <p>
-      Edited to React App by <a href="https://github.com/HasanRs">@HasanRs</a>
-    </p>
-  </footer>
     </div>
   )
 }
